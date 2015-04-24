46ad4fe527ba3e3d6d74634aa483386667d02e1d
Allen Coin <allencoin@gmail.com>
2015-04-24 08:35:54 +0000

    install FactoryGirls gem and integrate it into existing tests

---
+++ test/factories.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :quote do
+    author "Allen Coin"
+    saying "You only yolo once."
+  end
+end
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
   test "unique_tag" do
-  	quote = Quote.create(:author => 'Ken M', :saying => 'You only yolo once.')
-  	expected = 'KM#' + quote.id.to_s
+  	quote = FactoryGirl.create(:quote, :author => 'Allen Coin')
+  	expected = 'AC#' + quote.id.to_s
   	actual = quote.unique_tag
   	assert_equal expected, actual
   end
